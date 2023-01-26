4e2bfa3a2be6c1cf565fd21110459e90d96c6758
Michelle Too <mjxtcodes@gmail.com>
2023-01-25 16:27:46 -0800

    testing

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::UserSerializer
   attributes :id, :username, :admin
   has_many :tickets
   has_many :user_projects
-  has_many :projects, through: :user_projects
+  has_many :projects
   #fjdfkdaslfjksd
 end
