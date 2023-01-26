b3c6813535bb0f2677f3d85d2fe1f6158d7ebc2b
Michelle Too <mjxtcodes@gmail.com>
2023-01-25 16:38:52 -0800

    fixed serializer name

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
-class UserSerializer < ActiveModel::UserSerializer
+class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :admin
   has_many :tickets
   has_many :user_projects
